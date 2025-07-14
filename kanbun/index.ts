1432711bb2bb97ae610802ea3315cae89cee6903
hiroto-hara <xre0605@gmail.com>
2025-07-14 21:28:35 +0900

    feat: ファイルに日時を書き込む

--- kanbun/index.ts
+++ kanbun/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+// output.txtをファイルとして取得し, テキストを読み出す
+const file = Bun.file("output.txt");
+const source = await file.text();
+
+const now = new Date();
+
+// ファイルに元のテキストと改行・日時を書き込む
+const writer = file.writer();
+writer.write(source);
+writer.write("\n");
+writer.write(now.toString());
+writer.end();
+
+// ファイルから再びテキストを読み出す
+const result = await file.text();
+console.log(result);
