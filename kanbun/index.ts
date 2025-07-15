30e3b443ef24ced9693f674de6cf15de26f9a538
hiroto-hara <xre0605@gmail.com>
2025-07-15 12:15:29 +0900

    feat: コマンドライン引数の数による制御を行う

--- kanbun/index.ts
+++ kanbun/index.ts
@@ -2,16 +2,21 @@
 const file = Bun.file("output.txt");
 const source = await file.text();
 
-// コマンドライン引数の最後の文字列を取得する
-const memo: string = Bun.argv.pop() ?? "";
+if (Bun.argv.length === 3) {
+  // コマンドライン引数の最後の文字列を取得する
+  const memo: string = Bun.argv.pop() ?? "";
+  // ファイルに元のテキストと改行・日時を書き込む
+  const writer = file.writer();
+  writer.write(source);
+  writer.write("\n");
+  writer.write(memo);
+  writer.end();
 
-// ファイルに元のテキストと改行・日時を書き込む
-const writer = file.writer();
-writer.write(source);
-writer.write("\n");
-writer.write(memo);
-writer.end();
-
-// ファイルから再びテキストを読み出す
-const result = await file.text();
-console.log(result);
+  // ファイルから再びテキストを読み出す
+  const result = await file.text();
+  console.log(result);
+} else if (Bun.argv.length === 2) {
+  console.log(source);
+} else {
+  throw new Error("追加のコマンドライン引数は1つまでです");
+}
