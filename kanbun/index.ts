e6954cb5f0a21425b132cde9a355761acc1f72f1
hiroto-hara <xre0605@gmail.com>
2025-07-16 21:41:30 +0900

    feat: 関数切り出し・テスト作成

--- kanbun/index.ts
+++ kanbun/index.ts
@@ -1,3 +1,5 @@
+import { formatToItem } from "./format";
+
 // output.txtをファイルとして取得し, テキストを読み出す
 const file = Bun.file("output.txt");
 const source = await file.text();
@@ -5,11 +7,12 @@ const source = await file.text();
 if (Bun.argv.length === 3) {
   // コマンドライン引数の最後の文字列を取得する
   const memo: string = Bun.argv.pop() ?? "";
+  const item: string = formatToItem(memo);
   // ファイルに元のテキストと改行・日時を書き込む
   const writer = file.writer();
   writer.write(source);
   writer.write("\n");
-  writer.write(memo);
+  writer.write(item);
   writer.end();
 
   // ファイルから再びテキストを読み出す
