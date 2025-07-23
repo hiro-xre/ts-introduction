1e16155fd16a93fbc1544bcb53f564080adb934a
hiroto-hara <xre0605@gmail.com>
2025-07-23 22:58:13 +0900

    feat: DB登録処理を実装する

--- kanbun/db.ts
+++ kanbun/db.ts
@@ -9,3 +9,11 @@ export function initializeItemTable(db: Database) {
   const query = db.prepare(queryString);
   query.run();
 }
+
+type Kind = "memo" | "todo" | "done";
+
+export function createItem(db: Database, content: string, kind: Kind) {
+  const queryString = `INSERT INTO item (content, kind) VALUES (?, ?)`;
+  const query = db.query(queryString);
+  query.run(content, kind);
+}
--- kanbun/index.ts
+++ kanbun/index.ts
@@ -1,14 +1,28 @@
 import { Database } from "bun:sqlite";
-import { initializeItemTable } from "./db";
+import { initializeItemTable, createItem } from "./db";
 
 const db = new Database("sqlite.db");
 
 initializeItemTable(db);
 
 if (Bun.argv.length === 4) {
-  // コマンドライン引数の最後の文字列を取得する
+  // コマンドライン引数の最後2つの文字列を取得する
   const content: string = Bun.argv.pop() ?? "";
   const command: string = Bun.argv.pop() ?? "";
+
+  switch (command) {
+    case "memo":
+      createItem(db, content, "memo");
+      break;
+    case "todo":
+      createItem(db, content, "todo");
+      break;
+    case "done":
+      // TODO: doneの処理
+      break;
+    default:
+      throw new Error("不正なコマンドです");
+  }
 
   // console.log(result);
 } else if (Bun.argv.length === 2) {
