12d41e0604f798136a0a49afc9f5037bb779f60c
hiroto-hara <xre0605@gmail.com>
2025-08-16 22:52:13 +0900

    refactor: エラー処理を改善する

--- kanbun/index.ts
+++ kanbun/index.ts
@@ -13,43 +13,60 @@ const db = new Database("sqlite.db");
 
 initializeItemTable(db);
 
-if (Bun.argv.length === 4) {
-  // コマンドライン引数の最後2つの文字列を取得する
-  const content: string = Bun.argv.pop() ?? "";
-  const command: string = Bun.argv.pop() ?? "";
+try {
+  if (Bun.argv.length === 4) {
+    // コマンドライン引数の最後2つの文字列を取得する
+    const content: string = Bun.argv.pop() ?? "";
+    const command: string = Bun.argv.pop() ?? "";
 
-  switch (command) {
-    case "memo":
-      createItem(db, content, "memo");
-      break;
-    case "todo":
-      createItem(db, content, "todo");
-      break;
-    case "done":
-      updateTodoToDone(db, content);
-      break;
-    case "drop":
-      deleteItem(db, content);
-      break;
-    default:
-      throw new Error("不正なコマンドです");
+    switch (command) {
+      case "memo":
+        createItem(db, content, "memo");
+        break;
+      case "todo":
+        createItem(db, content, "todo");
+        break;
+      case "done":
+        updateTodoToDone(db, content);
+        break;
+      case "drop":
+        deleteItem(db, content);
+        break;
+      default:
+        throw new Error("不正なコマンドです");
+    }
+
+    printItems(db);
+  } else if (Bun.argv.length === 3) {
+    const command: string = Bun.argv.pop() ?? "";
+    switch (command) {
+      case "trim":
+        archiveItems(db);
+        break;
+      default:
+        throw new Error("不正なコマンドです");
+    }
+
+    printItems(db);
+  } else if (Bun.argv.length === 2) {
+    printItems(db);
+  } else {
+    throw new Error("追加のコマンドライン引数の数が多すぎます");
   }
-} else if (Bun.argv.length === 3) {
-  const command: string = Bun.argv.pop() ?? "";
-  switch (command) {
-    case "trim":
-      archiveItems(db);
-      break;
-    default:
-      throw new Error("不正なコマンドです");
+} catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message);
   }
-} else if (Bun.argv.length === 2) {
-  const items = getItems(db);
-  items.forEach((item) => {
-    console.log(formatToItem(item));
-  });
-} else {
-  throw new Error("追加のコマンドライン引数の数が多すぎます");
 }
 
 db.close();
+
+function printItems(db: Database) {
+  const items = getItems(db);
+  items.forEach((item) => {
+    if (item.archived) {
+      return;
+    }
+    console.log(formatToItem(item));
+  });
+}
